9784aa0979a481e1fe92ecfa852a1dc32fb35613
Aika586 <baatirbekovna.6@mail.ru>
2021-10-30 03:16:33 +0600

    header component built

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 
 import './App.css';
+import Title from './Title';
 
 function App() {
+
+
   return (
+
     <div className="App">
+      <Title/>
+    
+      <h1>Learn React</h1>
       
-          Learn React
+      </div>
         
-    </div>
+    
   );
 }
 
---
+++ src/Title.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import './Title.css';
+
+export default function Title() {
+    return (
+        <div className="Title">
+            <h1>CHICAGOTOUR</h1> 
+        
+        <div className="Title-Subtitle">Chicago Places to Visit</div>
+        </div>
+    )
+}
+
